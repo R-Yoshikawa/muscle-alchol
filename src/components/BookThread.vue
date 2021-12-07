6cde959071f5caf2f0e0f1285370de44e059fcd4
矢田 浩章 <N25279@tes.ns-sol.co.jp>
2021-12-07 14:51:33 +0900

    いいねボタン、投稿ボタン、投稿フォーム、タブ

--- src/components/BookThread.vue
+++ src/components/BookThread.vue
@@ -1,74 +1,77 @@
 <template>
 <div>
-<div style="border: 1px solid #F0F0F0; width: 90%; height: 160px; overflow: auto;">
-<!-- 折りたたみ展開ボタン -->
-<div onclick="obj=document.getElementById('menu1').style; obj.display=(obj.display=='none')?'block':'none';">
-<a style="cursor:pointer;">▼ スレッド</a>
-</div>
-<!--// 折りたたみ展開ボタン -->
-  
-<!-- ここから先を折りたたむ -->
-<div id="menu1" style="display:none;clear:both;">
- 
-<!--この部分が折りたたまれ、展開ボタンをクリックすることで展開します。-->
- <ul style="list-style: none;">
-   <li>yata333333</li>
-   <li>yasuda</li>
-   <li>yoshikawa</li>
-  <li>yata</li>
-   <li>yasuda</li>
-   <li>yoshikawa</li>
-  <li>yata</li>
-   <li>yasuda</li>
-   <li>yoshikawa</li>
- </ul>
-</div>
-<!--// ここまでを折りたたむ -->
-</div>
 
-<div style="border: 1px solid #F0F0F0; width: 90%; height: 160px; overflow: auto;">
-<!-- 折りたたみ展開ボタン -->
-<div onclick="obj=document.getElementById('menu2').style; obj.display=(obj.display=='none')?'block':'none';">
-<a style="cursor:pointer;">▼ レビュー</a>
-</div>
-<!--// 折りたたみ展開ボタン -->
-  
-<!-- ここから先を折りたたむ -->
-<div id="menu2" style="display:none;clear:both;">
- 
-<!--この部分が折りたたまれ、展開ボタンをクリックすることで展開します。-->
- <ul style="list-style: none;">
-   <li>yatayatayatayatayatayatayatayatayatayatayatayatayatayatayatayata 
-     <br>
-     yatayatayatayatayatayatayatayatayatayatayatayatayatayatayatayatayata
-   </li>
-   <li>yasuda</li>
-   <li>yoshikawa</li>
-   
-   <li>yata</li>
-   <li>yasuda</li>
-   <li>yoshikawa</li>
-   
-   <li>yata</li>
-   <li>yasuda</li>
-   <li>yoshikawa</li>
- </ul>
-</div>
-<!--// ここまでを折りたたむ -->
-</div>
-<form action="#" method="post">
+  <div class="tabbox">
+    <input type="radio" name="tabset" id="tabcheck1" checked><label for="tabcheck1" class="tab">スレッド</label>
+    <input type="radio" name="tabset" id="tabcheck2"        ><label for="tabcheck2" class="tab">レビュー</label>
+    <div class="tabcontent" id="tabcontent1">
+      <div style="border: 1px solid #F0F0F0; width: 90%; height: 160px; overflow: auto;">
+        <ul style="list-style: none;">
+        <li class="subtitle">
+          <div>p.20について</div>
+          <div>～～～の意味が分かりません.。XXXということでしょうか？</div>
+          <div style="text-align: right;">
+            <button>💛</button>
+          </div>
+        </li>
+        <li class="subtitle">
+          <div>教えてください</div>
+          <div>p。25の～～～は、どういう意味ですか</div><br>
+          <div style="text-align: right;">
+            <button>💛</button>
+          </div>
+        </li>
+        </ul>
+      </div>
+      <form action="#" method="post" style="text-align: center;">
+        <div style="text-align: center;">件名: <br>
+          <input type="text" name="content" style="display: inline-block;">
+        </div>
+        <p>入力内容：<br>
+        <textarea name="comment" cols="30" rows="5"></textarea></p>
+        <button type="submit">返信</button>
+      </form>
 
-	<p>入力内容：<br>
-<textarea name="comment" cols="30" rows="5"></textarea></p>
-</form>
-<button type="submit">返信</button>
-<form action="#" method="post">
+    </div>
+    <div class="tabcontent" id="tabcontent2">
+      <div style="border: 1px solid #F0F0F0; width: 90%; height: 160px; overflow: auto;">
+        <ul style="list-style: none;">
+        <li class="subtitle">
+          <div>タイトル</div>
+          <div>コンテント</div>
+          <div style="text-align: right;">
+            <button>💛</button>
+          </div>
+        </li>
+        <li class="subtitle">
+          <div>おもんない！</div>
+          <div>最悪でした</div><br>
+          <div style="text-align: right;">
+            <button>💛</button>
+          </div>
+        </li>
+        <li class="subtitle">
+          <div>絶対読むべき！</div>
+          <div>もっと早くこの本に出合いたかったです。</div>
+          <div style="text-align: right;">
+            <button>💛</button>
+          </div>
+        </li>
+        </ul>
+      </div>
 
-	<p>入力内容：<br>
-<textarea name="comment" cols="30" rows="5"></textarea></p>
-</form>
-<button>投稿</button>
-<button>いいね</button>
+    <form action="#" method="post" style="text-align: center;">
+        <div style="text-align: center;">件名: <br>
+          <input type="text" name="content" style="display: inline-block;">
+        </div>
+      <p>入力内容：<br>
+      <textarea name="comment" cols="30" rows="5"></textarea></p>
+      <button type="submit">返信</button>
+    </form>
+
+
+    </div>
+  </div>
 </div>
 
 
@@ -78,4 +81,62 @@
 
 </script>
 
+<style type="text/css">
+  /* ▼タブ機能の掲載領域の装飾(※必須ではありません) */
+   .tabbox { margin: 0px; padding: 1em; background-color: #f8f8ff; }
 
+   /* ▼タブ機能を制御するラジオボタン(非表示にする) */
+   .tabbox input { display: none; }
+
+   /* ▼タブ(共通装飾＋非選択状態の装飾) */
+   .tab {
+      display: inline-block;
+      border-width: 1px 1px 0px 1px;
+      border-style: solid;
+      border-color: black;
+      border-radius: 0.75em 0.75em 0px 0px;
+      padding: 0.75em 1em;
+      color: black;
+      background-color: #e0e0e0;
+      font-weight: bold;
+   }
+
+   /* ▼タブにマウスポインタが載った際の装飾 */
+   .tab:hover {
+      background-color: #ccffcc;
+      color: green;
+      cursor: pointer;
+   }
+
+   /* ▼チェックが入っているラジオボタンの隣にあるタブの装飾(＝選択状態のタブ) */
+   input:checked + .tab {
+      color: red;
+      background-color: #ffffcc;
+      position: relative;
+      z-index: 10;
+   }
+
+   /* ▼タブの中身(共通装飾＋非選択状態の装飾) */
+   .tabcontent {
+      display: none;
+      border: 1px solid black;
+      margin-top: -1px;
+      padding: 1em;
+      position: relative;
+      z-index: 0;
+      background-color: #ffffcc;
+      text-align: left;
+   }
+
+   /* ▼チェックが入っているラジオボタンに対応するタブの中身を表示する */
+   #tabcheck1:checked ~ #tabcontent1 { display: block; }
+   #tabcheck2:checked ~ #tabcontent2 { display: block; }
+   #tabcheck3:checked ~ #tabcontent3 { display: block; }
+
+   .subtitle {
+     border-style: solid;
+     border-width: 1px 1px 1px 1px;
+     padding: 5px;
+     margin: 5px;
+   }
+</style>
