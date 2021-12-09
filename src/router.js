51eb489ee5697596c96ccff599ad8a0e44e7fe48
Takumi <ohnuki.takumi.q5d@jp.nssol.nipponsteel.com>
2021-12-09 14:52:04 +0900

    resolve image bag

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Book from "./components/Book.vue";
-import Mypage from "./components/Mypage.vue";
+import Mypage from "./components/MyPage.vue";
 Vue.use(Router);
 
 export default new Router({
